5ddd02b1fc833b25036d0dd2a3fe81118b6d3d4f
barif <w3db@yandex.ru>
2013-03-18 20:50:47 +0200

    joins

--- src/Knws/PortfolioBundle/Entity/ProductRepository.php
+++ src/Knws/PortfolioBundle/Entity/ProductRepository.php
@@ -29,4 +29,20 @@ class ProductRepository extends EntityRepository
             ->createQuery('SELECT p FROM KnwsPortfolioBundle:Product p ORDER BY p.name ASC')
             ->getResult();
     }
+
+    public function findOneByIdJoinedToCategory($id)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery('
+                SELECT p, c FROM KnwsPortfolioBundle:Product p
+                JOIN p.category c
+                WHERE p.id = :id'
+            )->setParameter('id', $id);
+
+        try {
+            return $query->getSingleResult();
+        } catch (\Doctrine\ORM\NoResultException $e) {
+            return null;
+        }
+    }
 }
