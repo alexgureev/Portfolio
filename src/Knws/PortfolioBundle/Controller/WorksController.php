1ad0ab69f7688dbc455d44b640cfa1dbab7f8542
barif <w3db@yandex.ru>
2013-03-20 22:12:26 +0200

    navigation model

--- src/Knws/PortfolioBundle/Controller/WorksController.php
+++ src/Knws/PortfolioBundle/Controller/WorksController.php
@@ -9,12 +9,7 @@ class WorksController extends Controller
     public function indexAction()
     {
         $content = array(
-            'navigation' => array(
-                'works' => array('asset' => 'knws_portfolio_works', 'label' => 'работы', 'active' => true),
-                'services' => array('asset' => 'knws_portfolio_services', 'label' => 'услуги', 'active' => false),
-                'skills' => array('asset' => 'knws_portfolio_skills', 'label' => 'навыки', 'active' => false),
-                'contacts' => array('asset' => 'knws_portfolio_contacts', 'label' => 'контакты', 'active' => false)
-            ),
+            'navigation' => $this->navigationAction('knws_portfolio_works'),
             'works' => array(
                 'knws' => array(
                     'class' => 'grid_2 prefix_2',
@@ -33,30 +28,45 @@ class WorksController extends Controller
         return $this->render('KnwsPortfolioBundle:Works:index.html.twig', $content);
     }
 
-    public function workAction()
+    public function workAction($slug)
     {
         $content = array(
-            'navigation' => array(
-                'works' => array('asset' => 'knws_portfolio_works', 'label' => 'работы', 'active' => true),
-                'services' => array('asset' => 'knws_portfolio_services', 'label' => 'услуги', 'active' => false),
-                'skills' => array('asset' => 'knws_portfolio_skills', 'label' => 'навыки', 'active' => false),
-                'contacts' => array('asset' => 'knws_portfolio_contacts', 'label' => 'контакты', 'active' => false)
-            ),
+            'navigation' => $this->navigationAction(null),
             'works' => array(
-                'knws' => array(
-                    'class' => 'grid_2 prefix_2',
-                    'slug' => 'knws',
-                    'assetTitle' => 'knws',
-                    'date' => '2013',
-                    'tags' => //array('show' => false)
-                              array('class' => 'tag php', 'url' => 'http://php.net', 'title' => 'PHP', 'show' => true),
-                    'title' => 'KNWS framework',
-                    'description' => 'MVC фреймворк с использованием ORM Doctrine и Symfony Components, позволяет создавать многофункциональные веб-приложения и устанавливать их на сервер при помощи Composer.'
-                ),
+                'class' => 'grid_2 prefix_2',
+                'url' => 'https://github.com/barif/knws',
+                'slug' => $slug,
+                'assetTitle' => 'knws',
+                'date' => '2013',
+                'tags' => //array('show' => false)
+                          array('class' => 'tag php', 'url' => 'http://php.net', 'title' => 'PHP', 'show' => true),
+                'title' => 'KNWS framework',
+                'description' => '<p class="text-left">MVC фреймворк с использованием ORM Doctrine и Symfony Components, позволяет создавать многофункциональные веб-приложения и устанавливать их на сервер при помощи Composer.</p><p class="text-left">Реализация: OOP-PHP.</p>'
             ),
-            'title' => 'Выполненые работы'
+            'title' => 'KNWS framework'
         );
 
         return $this->render('KnwsPortfolioBundle:Works:work.html.twig', $content);
+    }
+
+    public function navigationAction($asset)
+    {
+        $navigation = array(
+                'works' => array('asset' => 'knws_portfolio_works', 'label' => 'работы'),
+                'services' => array('asset' => 'knws_portfolio_services', 'label' => 'услуги'),
+                'skills' => array('asset' => 'knws_portfolio_skills', 'label' => 'навыки'),
+                'contacts' => array('asset' => 'knws_portfolio_contacts', 'label' => 'контакты')
+            );
+
+        foreach($navigation as $key => $val)
+        {
+            if($asset == $val['asset']) {
+                $navigation[$key]['active'] = true;
+            } else {
+                $navigation[$key]['active'] = false;
+            }
+        }
+
+        return $navigation;
     }
 }
